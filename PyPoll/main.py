7b60fc8a8897d4e8f906d59a34c5d7fecaacca80
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-19 14:23:35 -0600

    Imported and opened PyPoll CSVfile for reading

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,14 @@
-print ("Hello from PyPoll")
+#imports operating system module
+import os 
+
+#imports module for reading CSV files
+import csv
+
+csvpath = os.path.join ("Resources", "election_data.csv")
+print (csvpath)
+
+#open CSV file for reading
+with open (csvpath) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader (csvfile, delimiter = ',')
+    print (csvreader)
