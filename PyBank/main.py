b348f0acddc6571a22f86b674ca4831d4d88d936
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-19 15:42:24 -0600

    Printed out the rows in both folders

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,3 +13,22 @@ with open (csvpath) as csvfile:
     csvreader = csv.reader (csvfile, delimiter = ',')
     print (csvreader)
 
+# Read the header row first 
+    csv_header = next(csvfile)
+    print(f"Header: {csv_header}")
+
+    for row in csvreader:
+        print (row)
+
+#The total number of months included in the dataset
+
+
+
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in losses (date and amount) over the entire period
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,3 +12,20 @@ with open (csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader (csvfile, delimiter = ',')
     print (csvreader)
+
+# Read the header row first 
+    csv_header = next(csvfile)
+    print(f"Header: {csv_header}")    
+
+    for row in csvreader:
+        print (row)
+
+#The total number of votes cast
+
+#A complete list of candidates who received votes
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
