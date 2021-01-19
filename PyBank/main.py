1866d76bedaa090011c380cc5aaee1cfd4d6660f
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-19 14:16:24 -0600

    Added given CSV files in Resources folders of PyBank and PyPoll. Imported and opened CSVfile of PyBank for reading.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,15 @@
-print ("Hello from PyBank")
+#imports operating system module
+import os 
+
+#imports module for reading CSV files
+import csv
+
+csvpath = os.path.join ("Resources", "budget_data.csv")
+print (csvpath)
+
+#open CSV file for reading
+with open (csvpath) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader (csvfile, delimiter = ',')
+    print (csvreader)
+
