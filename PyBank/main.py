6233619ebd0f3ad9d257fc001c5ddbce10bdff9b
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-20 21:57:44 -0600

    Added for-loop to calculate the total months and profit/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,36 +1,48 @@
+
 #imports operating system module
 import os 
 
 #imports module for reading CSV files
 import csv
 
+total_ProfitLoss = 0  
 csvpath = os.path.join ("Resources", "budget_data.csv")
-# print (csvpath)
+#print (csvpath)
 
 #open CSV file for reading
 with open (csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader (csvfile, delimiter = ',')
-    # print (csvreader)
+    #print (csvreader)
 
 # Read the header row first 
     csv_header = next(csvfile)
-    # print(f"Header: {csv_header}")
+    #print(f"Header: {csv_header}")
 
     # for row in csvreader:
     #     print (row)
+   
+    for row in csvreader:
+        #The total number of months included in the dataset
+        total = len (list(csvreader))
+        print(f"Total Months: {total}")
+        #The net total amount of "Profit/Losses" over the entire period
+        total_ProfitLoss += int(row[1])
+        print (f'Total: $ {total_ProfitLoss}')  
+   
+   
 
-    #The total number of months included in the dataset
-    total = len (list(csvreader))
-    # print (total)
-    print(f"Total Months: {total}")
+
+      
 
 
 
 
 
 
-#The net total amount of "Profit/Losses" over the entire period
+
+
+
 
 #Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 
