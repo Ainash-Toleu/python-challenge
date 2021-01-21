20af789983391d18a9fb4dc5e96506a688676245
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-20 23:19:36 -0600

    Calculated the total of month and total of profit/losses right, cleaned the code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,28 @@
-
 #imports operating system module
-import os 
-
-#imports module for reading CSV files
+import os
+#imports module for reading csv files
 import csv
 
-total_ProfitLoss = 0  
+#sets the path for file
 csvpath = os.path.join ("Resources", "budget_data.csv")
-#print (csvpath)
 
-#open CSV file for reading
+#creates a list to store data
+total_month = []
+#creates a variable with an integer
+total_profit_losses = 0
+
+#open the csv file for reading
 with open (csvpath) as csvfile:
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader (csvfile, delimiter = ',')
-    #print (csvreader)
-
-# Read the header row first 
-    csv_header = next(csvfile)
-    #print(f"Header: {csv_header}")
-
-    # for row in csvreader:
-    #     print (row)
-   
+    #csv reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter =',')
+    #skip the header
+    next(csvfile)
+    #loops through csvreader
     for row in csvreader:
         #The total number of months included in the dataset
-        total = len (list(csvreader))
-        print(f"Total Months: {total}")
-        #The net total amount of "Profit/Losses" over the entire period
-        total_ProfitLoss += int(row[1])
-        print (f'Total: $ {total_ProfitLoss}')  
-   
-   
-
-
-      
-
-
-
-
-
-
-
-
-
-
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-#The greatest increase in profits (date and amount) over the entire period
-
-#The greatest decrease in losses (date and amount) over the entire period
+        total_month.append(row[0])
+        # The net total amount of "Profit/Losses" over the entire period
+        total_profit_losses += int(row[1])
+    #print out the result
+    print (f'Total Months: {len(total_month)}')
+    print (f'Total: $ {total_profit_losses}')   
