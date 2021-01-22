d9e455a0a96ea441124c905f16f8ba67d660755e
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-01-22 00:46:33 -0600

    Calculated average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,10 +7,11 @@ import csv
 csvpath = os.path.join ("Resources", "budget_data.csv")
 
 #creates a list to store data
-total_month = []
+months_total = []
+monthly_profit_losses = []
+
 #creates a variable with an integer
 net_profit_losses = 0
-total_profit_loses = []
 
 #open the csv file for reading
 with open (csvpath) as csvfile:
@@ -21,15 +22,21 @@ with open (csvpath) as csvfile:
     #loops through csvreader
     for row in csvreader:
         #The total number of months included in the dataset
-        total_month.append(row[0])
+        months_total.append(row[0])
         # The net total amount of "Profit/Losses" over the entire period
         net_profit_losses += int(row[1])
+        #add all the data in row 2 to a list
+        monthly_profit_losses.append(row[1])
+        monthly_changes = [int(x) - int(monthly_profit_losses[i - 1]) for i, x in enumerate(monthly_profit_losses) if i > 0]
+        sum_changes = sum(monthly_changes)
         
-        total_profit_loses.append(row[1])
         
-        
+ #print out the result
+    average_changes = round(sum_changes/len(monthly_changes), 2)
+    print (f'Total Months: {len(months_total)}')
+    print (f'Total: $ {net_profit_losses}')  
+    print (f'Average  Change: $ {average_changes}')
+    # print (len(monthly_changes))
+    # print (sum_changes)
     
-    #print out the result
-    print (total_profit_loses[2])
-    print (f'Total Months: {len(total_month)}')
-    print (f'Total: $ {net_profit_losses}')   
+
